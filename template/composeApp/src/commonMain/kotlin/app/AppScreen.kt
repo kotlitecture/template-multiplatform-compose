66d2b33f7a538a39cf796b05ec6179a3796721fb
Aleksha <kotlitecture@gmail.com>
2024-04-22 16:51:38 +0300

    init (*) remove imports

--- template/composeApp/src/commonMain/kotlin/app/AppScreen.kt
+++ template/composeApp/src/commonMain/kotlin/app/AppScreen.kt
@@ -1,6 +1,5 @@
 package app
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
