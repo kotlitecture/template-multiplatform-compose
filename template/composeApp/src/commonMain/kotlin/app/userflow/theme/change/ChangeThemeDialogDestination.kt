70cdba2d76632fe5a7183ac08172fe45d42921d0
Aleksha <kotlitecture@gmail.com>
2024-04-24 13:45:33 +0300

    fix (*) crash

--- template/composeApp/src/commonMain/kotlin/app/userflow/theme/change/ChangeThemeDialogDestination.kt
+++ template/composeApp/src/commonMain/kotlin/app/userflow/theme/change/ChangeThemeDialogDestination.kt
@@ -4,7 +4,6 @@ import androidx.navigation.NavGraphBuilder
 import core.ui.navigation.ArgsStrategy
 import core.ui.navigation.NavigationDestination
 import core.ui.navigation.NavigationStrategy
-import kotlinx.serialization.builtins.serializer
 
 /**
  * Navigation destination for the change theme dialog screen.
@@ -13,7 +12,7 @@ object ChangeThemeDialogDestination : NavigationDestination<Unit>() {
 
     override val id: String = "change_theme_dialog_screen"
     override val navStrategy: NavigationStrategy = NavigationStrategy.NewInstance
-    override val argsStrategy: ArgsStrategy<Unit> = ArgsStrategy.json(Unit.serializer())
+    override val argsStrategy: ArgsStrategy<Unit> = ArgsStrategy.noArgs()
     override fun doBind(builder: NavGraphBuilder) = dialog(builder) { ChangeThemeDialog() }
 
 }
