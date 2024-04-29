4dfab5734432b9241c890f5ba5c335cbbca89095
Aleksha <kotlitecture@gmail.com>
2024-04-29 09:32:49 +0300

    fix (*) removed unused import

--- template/composeApp/build.gradle.kts
+++ template/composeApp/build.gradle.kts
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalWasmDsl::class)
-
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
