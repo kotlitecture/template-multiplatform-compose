8b697b04704a7dc1346219e6d933882fe30d2ed7
Aleksha <kotlitecture@gmail.com>
2024-04-30 21:57:27 +0300

    fix (*) initial size

--- template/composeApp/src/jvmMain/kotlin/main.kt
+++ template/composeApp/src/jvmMain/kotlin/main.kt
@@ -1,5 +1,9 @@
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import app.App
 import org.jetbrains.compose.resources.stringResource
 import template.composeapp.generated.resources.Res
@@ -8,7 +12,10 @@ import template.composeapp.generated.resources.app_name
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = stringResource(Res.string.app_name)
+        title = stringResource(Res.string.app_name),
+        state = rememberWindowState(
+            size = DpSize(480.dp, 640.dp)
+        )
     ) {
         App()
     }
