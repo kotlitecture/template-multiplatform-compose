7e64e56b1ba838ceea2202f7742684b0793e2f46
Aleksha <kotlitecture@gmail.com>
2024-05-03 16:44:18 +0300

    fix (*) issues + screenshots

--- template/composeApp/src/androidMain/kotlin/kotli/app/AppActivity.kt
+++ template/composeApp/src/androidMain/kotlin/kotli/app/AppActivity.kt
@@ -1,16 +1,33 @@
 package kotli.app
 
 import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotli.app.di.instance
 import shared.core.misc.extensions.findActivity
 import shared.core.navigation.NavigationState
 import shared.core.theme.ThemeState
+
+class AppActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+            EdgeToEdgeHandler(instance())
+            SplashBlock(splashScreen, instance())
+        }
+    }
+}
 
 @Composable
 private fun EdgeToEdgeHandler(state: ThemeState) {
