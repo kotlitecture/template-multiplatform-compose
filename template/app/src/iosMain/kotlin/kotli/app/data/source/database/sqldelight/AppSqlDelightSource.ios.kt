8f4a43c463c417501ffec24c5c44b6bec44642a6
Aleksha <kotlitecture@gmail.com>
2024-07-17 21:30:14 +0300

    refac

--- template/app/src/iosMain/kotlin/kotli/app/data/source/database/sqldelight/AppSqlDelightSource.ios.kt
+++ template/app/src/iosMain/kotlin/kotli/app/data/source/database/sqldelight/AppSqlDelightSource.ios.kt
@@ -3,7 +3,6 @@ package kotli.app.data.source.database.sqldelight
 import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
-import kotli.app.data.source.database.sqldelight.AppDatabase
 
 actual fun createSqlDriver(name: String): SqlDriver {
     return NativeSqliteDriver(AppDatabase.Schema.synchronous(), name)
