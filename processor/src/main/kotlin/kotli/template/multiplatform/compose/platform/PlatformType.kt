c1d1114c7349c44cd5454313f52d7ff0821e3d0c
Aleksha <kotlitecture@gmail.com>
2024-07-21 14:07:52 +0300

    release (0.8.1) reorder features

--- processor/src/main/kotlin/kotli/template/multiplatform/compose/platform/PlatformType.kt
+++ processor/src/main/kotlin/kotli/template/multiplatform/compose/platform/PlatformType.kt
@@ -6,7 +6,7 @@ import java.net.URL
 
 object PlatformType : FeatureType {
     override fun getId(): String = "platform"
-    override fun getOrder(): Int = 0
+    override fun getOrder(): Int = -2
     override fun getIcon(): URL? = ResourceUtils.get(this, "feature_type_platform.svg")
     override fun getTitle(): String? = ResourceUtils.getAsString(this, "feature_type_platform_title.md")
     override fun getDescription(): String? = ResourceUtils.getAsString(this, "feature_type_platform_description.md", true)
