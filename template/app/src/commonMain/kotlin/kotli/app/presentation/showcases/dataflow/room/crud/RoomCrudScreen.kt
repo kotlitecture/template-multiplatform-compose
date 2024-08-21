235fbede6717d3a54129d57c8096ab57a54bc827
Aleksha <kotlitecture@gmail.com>
2024-08-21 21:07:19 +0300

    updated color scheme

--- template/app/src/commonMain/kotlin/kotli/app/presentation/showcases/dataflow/room/crud/RoomCrudScreen.kt
+++ template/app/src/commonMain/kotlin/kotli/app/presentation/showcases/dataflow/room/crud/RoomCrudScreen.kt
@@ -53,7 +53,7 @@ fun RoomCrudScreen() {
 @Composable
 private fun ActionsBlock(viewModel: RoomCrudViewModel) {
     AppOutlinedButton(
-        modifier = Modifier.padding(16.dp),
+        modifier = Modifier.padding(horizontal = 16.dp),
         onClick = viewModel::onAdd,
         text = "Add user"
     )
