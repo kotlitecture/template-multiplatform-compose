279e80214647ad0a471377e775db7459de4ebc77
Aleksha <kotlitecture@gmail.com>
2024-09-09 20:59:20 +0300

    [ci skip] gemini

--- template/app/src/commonMain/kotlin/kotli/app/App.kt
+++ template/app/src/commonMain/kotlin/kotli/app/App.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import kotli.app.di.get
 import kotli.app.presentation.app.AppScreen
-import kotli.app.presentation.passcode.PasscodeProvider
 import kotli.app.presentation.theme.AppThemeProvider
 import shared.presentation.viewmodel.ViewModelProvider
 
--- template/app/src/commonMain/kotlin/kotli/app/data/source/ai/AiSource.kt
+++ template/app/src/commonMain/kotlin/kotli/app/data/source/ai/AiSource.kt
@@ -1,9 +1,8 @@
 package kotli.app.data.source.ai
 
 import kotlinx.coroutines.flow.Flow
-import shared.data.source.DataSource
 
-interface AiSource : DataSource {
+interface AiSource {
 
     suspend fun generateContent(prompt: String): Flow<String>
 
