bea7cbc261b37fc9a7e780275ef3bb98331486e9
Mrljdx <mrljdx@qq.com>
2025-02-27 20:36:37 +0800

    fix ksp update iosApp build error
    https://github.com/google/ksp/blob/main/docs/ksp2.md#ksp1-deprecation-schedule

--- template/app/src/iosMain/kotlin/kotli/app/di/platform/createDataStorePath.kt
+++ template/app/src/iosMain/kotlin/kotli/app/di/platform/createDataStorePath.kt
@@ -1,10 +1,12 @@
 package kotli.app.di.platform
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSURL
 import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 actual fun createDataStorePath(fileName: String): String {
     val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
         directory = NSDocumentDirectory,
